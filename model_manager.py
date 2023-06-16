6bbab6b2ca480d25df07860e85b9ed3632ed745d
MasayoshiTsutsui <tsutsui-masayoshi0528@g.ecc.u-tokyo.ac.jp>
2023-06-16 09:53:56 +0000

    test save_as_file

--- model_manager.py
+++ model_manager.py
@@ -1,6 +1,7 @@
 import os
 import json
 import numpy as np
+import coremltools
 from constants import ACCUM_CLIENT_NUM
 
 GLOBAL_MODEL_DIR = '/home/tsuts/fl_server/global_model'
@@ -18,6 +19,17 @@ def save_as_file(params: dict) -> None:
     json.dump(params, json_fileptr)
     json_fileptr.close()
 
+    
+def test_save_as_file() -> None:
+
+    params = {
+        "weights": [1, 2, 3, 4, 5],
+        "biases": [5, 4, 3, 2, 1]
+    }
+
+    save_as_file(params)
+
+
 def try_to_update_model() -> bool:
     # if the number of uploaded params-set is less than 5, abort updating
     if len(os.listdir(CUR_UPLOADED_DIR)) < ACCUM_CLIENT_NUM:
@@ -34,15 +46,20 @@ def aggregate_and_update_model() -> None:
     weights = [params_dict["weights"] for params_dict in params_dict_list]
     biases = [params_dict["biases"] for params_dict in params_dict_list]
 
-    mean_weights = np.mean(weights, axis=0)
-    mean_biases = np.mean(biases, axis=0)
+    mean_weights = np.mean(weights, axis=0, dtype=np.float32)
+    mean_biases = np.mean(biases, axis=0, dtype=np.float32)
 
     # update model
+    # load "HandsTuri.mlmodel" as model
+    model = coremltools.models.MLModel(os.path.join(GLOBAL_MODEL_DIR, "HandsTuri.mlmodel"))
+
+    # update weights and biases of the last fully connected layer
+    model._spec.neuralNetwork.layers[-2].innerProduct.weights.floatValue = mean_weights
+    model._spec.neuralNetwork.layers[-2].innerProduct.bias.floatValue = mean_biases
+
+    # save the model as new HandsTuri.mlmodel file
+    model.save(os.path.join(GLOBAL_MODEL_DIR, "HandsTuri_v2.mlmodel"))
 
 
-
-    
-
-
-
-
+if __name__ == "__main__":
+    test_save_as_file()
